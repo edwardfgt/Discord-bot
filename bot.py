56928217ded97a03200dc58d8168e37d64a91233
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-05-17 16:26:27 +0100

    Update variable naming and set intents message_content to true so bot can view messages in channel

--- bot.py
+++ bot.py
@@ -5,10 +5,11 @@ import responses
 from config.config_files import APIkeys
 
 intents = Intents.default()
+intents.message_content = True
 
 async def send_message(message, user_message):
     try:
-        response = response.handle_response(user_message)
+        response = responses.get_response(user_message)
         await message.channel.send(response)
     except Exception as e:
         print(e)
@@ -19,5 +20,16 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f"{client.user} is now running")
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+
+        print(f"{username} said: {user_message}")
+        await send_message(message, user_message)
     
     client.run(APIkeys.TOKEN)
--- responses.py
+++ responses.py
@@ -1,7 +1,7 @@
-def handle_response(message) -> str:
+def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message == "test":
+    if p_message == "hello bot":
         return "Journals Butler, At your service."
 
     if p_message == "dan":
