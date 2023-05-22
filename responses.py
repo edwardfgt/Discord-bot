f4026212c55faf5c87e0a71dc2a10f00e8b72fe7
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-05-22 22:55:47 +0100

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,8 +1,11 @@
 def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message == "hello bot":
+    if p_message == "?bot":
         return "Journals Butler, At your service."
 
+    if p_message == "?":
+        return "?"
+        
     if p_message == "dan":
         return "Smells!"
